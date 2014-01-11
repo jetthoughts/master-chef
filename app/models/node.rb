f160ae9b7c70d94af93a32c79017939c15b74ef9
Valeriy Mironov <valeronm@gmail.com>
2014-01-11 19:32:07 +0200

    Added validation of node credentials is valid YAML

--- app/models/node.rb
+++ app/models/node.rb
@@ -2,4 +2,13 @@ class Node < ActiveRecord::Base
   belongs_to :project, inverse_of: :nodes
   has_many :deployments
   validates :name, presence: true, uniqueness: { scope: :project_id }
+  validate :validate_credentials
+
+  private
+
+  def validate_credentials
+    self.errors.add(:credentials, 'need to be hash in YAML') unless YAML.load(credentials).instance_of? Hash
+  rescue Psych::SyntaxError
+    self.errors.add(:credentials, 'need to be valid YAML')
+  end
 end
