0ce09d29866e577da6614232e354321d7cb0c435
Slavik Balabolka <bolkabolka@gmail.com>
2014-01-11 17:45:54 +0200

    roles CRUD as nested resouce

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,2 +1,51 @@
-class RolesController < InheritedResources::Base
+class RolesController < ApplicationController
+
+  before_action :load_project, except: [:show, :edit, :update, :destroy]
+  before_action :load_role, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @roles = @project.roles
+  end
+
+  def new
+    @role = @project.roles.build
+  end
+
+  def create
+    @role = @project.roles.build role_attributes
+    if @role.save
+      redirect_to project_roles_path(@project), notice: 'Role successfully created'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @role.update_attributes role_attributes
+      redirect_to project_roles_path(@role.project), notice: 'Role successfully updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @role.destroy
+
+    redirect_to project_roles_path(@role.project), notice: 'Role successfully deleted'
+  end
+
+  private
+
+  def role_attributes
+    params.require(:role).permit(:name, :config)
+  end
+
+  def load_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def load_role
+    @role = Role.find(params[:id])
+  end
+
 end
