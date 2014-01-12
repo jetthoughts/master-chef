51d25899ffe46217141198f035e0b4321f20c23c
Michael Nikitochkin <miry.sof@gmail.com>
2014-01-12 11:24:38 +0200

    :panda_face: updated deploy worker.

--- app/workers/deploy_worker.rb
+++ app/workers/deploy_worker.rb
@@ -6,9 +6,14 @@ class DeployWorker < Struct.new(:id)
 
   def perform
     deployment = Deployment.find(id)
+    Rails.logger.info '>>>>>>>>>> before deploy'
     deployment.deploy!
+    Rails.logger.info '>>>>>>>>>> before update success'
     deployment.update! success: true
   rescue StandardError => ex
+    Rails.logger.info '>>>>>>>>>> EXCEPTION:'
+    Rails.logger.info ex
+    Rails.logger.info ex.backtrace.join("\n")
     deployment.update! state: 'processing', success: false
     deployment.logger.append_log ex.message
   ensure
