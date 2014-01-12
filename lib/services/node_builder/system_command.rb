a3a0b34ca0200c02a5d71f1c932ced8a480db79a
Michael Nikitochkin <miry.sof@gmail.com>
2014-01-12 11:51:50 +0200

    :panda_face: move Gemfile of the chef to template.

--- lib/services/node_builder/system_command.rb
+++ lib/services/node_builder/system_command.rb
@@ -16,4 +16,8 @@ module SystemCommand
   def berkshelf_update_cookbooks
     system_cmd 'bundle exec berks install --path cookbooks'
   end
+
+  def environment
+    { 'BUNDLE_GEMFILE' => Rails.root.join('GemfileChef').to_s }
+  end
 end
