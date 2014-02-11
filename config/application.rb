5aeac4f1aab6c86ff02e42d6ad3f7a6834128cc9
Michael Nikitochkin <miry.sof@gmail.com>
2014-02-12 00:17:04 +0200

    :panda_face: Use cdn version of bootstrap css. Removed devise layout.

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module MasterChef
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.assets.precompile += %w(ace/*)
+    config.assets.precompile += %w(ace/* landing.js landing.css)
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
