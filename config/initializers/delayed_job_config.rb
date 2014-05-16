31bcfb38307149e1e25ce3c87bd04c4cf20a0ac0
Michael Nikitochkin <miry.sof@gmail.com>
2014-05-16 12:42:01 +0300

    Increased the max time run.

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,6 +1,6 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay         = 60
-Delayed::Worker.max_run_time        = 20.minutes
+Delayed::Worker.max_run_time        = 2.hours
 Delayed::Worker.read_ahead          = 10
 Delayed::Worker.delay_jobs          = !Rails.env.test?
 
