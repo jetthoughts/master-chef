7b6bc617f956c7d6bac5316b5880622580a5e242
Michael Nikitochkin <miry.sof@gmail.com>
2014-09-13 22:56:44 +0300

    Updtaed newrelci and decreased run time of job.

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,6 +1,6 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay         = 60
-Delayed::Worker.max_run_time        = 2.hours
+Delayed::Worker.max_run_time        = 1.hours
 Delayed::Worker.read_ahead          = 10
 Delayed::Worker.delay_jobs          = !Rails.env.test?
 
