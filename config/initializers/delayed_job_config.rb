a6b8a6697bba561e9d5ab9d9dad30bb3c2e79380
Michael Nikitochkin <miry.sof@gmail.com>
2015-03-15 17:13:36 +0100

    Updated ruby and DJ config

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,10 +1,6 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay         = 60
-Delayed::Worker.max_run_time        = 1.hours
+Delayed::Worker.max_run_time        = 2.hours
 Delayed::Worker.read_ahead          = 10
 Delayed::Worker.delay_jobs          = !Rails.env.test?
-
-# In production have default number of attempts ( which I think is 30 )
-if !Rails.env.production?
-  Delayed::Worker.max_attempts        = 3
-end
+Delayed::Worker.max_attempts        = 1
